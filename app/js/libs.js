9968fd94ee0b8735bd8aa89e99b74c698cfb7b2c
risedack1 <risedack1@gmail.com>
2021-09-23 21:35:54 +0300

    added contacts and projects

--- app/js/libs.js
+++ app/js/libs.js
@@ -1,15 +1,15 @@
 let inputPhone = document.querySelectorAll('input[type="tel"]');
 let popupForm = document.querySelectorAll('.popup__form')
 
-new fullpage('#fullpage', {
-    sectionSelector: '.full-page',
-    navigation: true,
-    responsiveHeight: 900,
-    responsiveWidth: 1490,
-});
+// new fullpage('#fullpage', {
+//     sectionSelector: '.full-page',
+//     navigation: true,
+//     responsiveHeight: 900,
+//     responsiveWidth: 1490,
+// });
 
-//methods
-fullpage_api.setAllowScrolling(true);
+// //methods
+// fullpage_api.setAllowScrolling(true);
 
 // -----------------------------------------------------------------
 
